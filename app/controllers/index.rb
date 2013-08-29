1dee7e58e80b75814b42c6aadf377f7cc3826597
richard bluestein <richard@bluestein.com>
2013-08-29 14:58:15 -0500

    slightly working. needs auth

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,10 +1,38 @@
 ### get requests ####
+enable :sessions
 
 get '/' do
   # Look in app/views/index.erb
+  	p params
+
   erb :index
 end
 
+get '/newuser' do
+		p params
+
+  erb :newuser
+end
+
+
 ### post requests ###
 
+post '/login' do
+  user_info = params[:form]
+  # the following works - returns user object if valid else nil
 
+  if User.authenticate(user_info[:email],user_info[:password]) 
+  @user = User.authenticate(user_info[:email],user_info[:password])
+  session[:user_id] = @user.id
+  erb :somewhere else
+else
+  erb :index
+end
+end
+
+post '/new_user_process' do
+
+	p params[:form]
+	newuser = User.create(params[:form])
+	erb :new_user_process
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
   # e.g., User.authenticate('jesse@devbootcamp.com', 'apples123')
   def self.authenticate(email, password)
-  	
+  	user = User.where(email: email).last
+  	p user.password
+  	p password
+  	user.password == password ? user : nil
+    #return user if user.password == password 
+    
     # if email and password correspond to a valid user, return that user
     # otherwise, return nil
   end
